18c3d65448a1bf3b6ab6990d1fdfaeb5fe706afb
Sean Reilly <sean@hatchet.run>
2024-11-08 12:15:09 -0800

    lets run a load test and build right

--- src/hatchet-load-test/emitter/main.go
+++ src/hatchet-load-test/emitter/main.go
@@ -30,10 +30,35 @@ func main() {
 	}
 
 	interrupt := cmdutils.InterruptChan()
+	c, err := client.New()
 
-	cleanup, err := run()
 	if err != nil {
 		panic(err)
+	}
+	cleanup, err := run(c)
+	if err != nil {
+		panic(err)
+	}
+
+	testEvent := loadTestEvent{
+
+		Data: map[string]string{
+			"test": "test",
+		},
+	}
+
+	log.Printf("pushing event hatchet:load-test")
+	// push an event
+	err = c.Event().Push(
+		context.Background(),
+		"hatchet:ha:load-test:v2",
+		testEvent,
+		client.WithEventMetadata(map[string]string{
+			"hello": "loadtest",
+		}),
+	)
+	if err != nil {
+		panic(fmt.Errorf("error pushing event: %w", err))
 	}
 
 	<-interrupt
@@ -43,12 +68,7 @@ func main() {
 	}
 }
 
-func run() (func() error, error) {
-	c, err := client.New()
-
-	if err != nil {
-		return nil, fmt.Errorf("error creating client: %w", err)
-	}
+func run(c client.Client) (func() error, error) {
 
 	w, err := worker.NewWorker(
 		worker.WithClient(
@@ -61,8 +81,8 @@ func run() (func() error, error) {
 
 	err = w.RegisterWorkflow(
 		&worker.WorkflowJob{
-			On:          worker.Events("hatchet:load-test"),
-			Name:        "load",
+			On:          worker.Events("hatchet:ha:load-test:v2"),
+			Name:        "ha-loadtester-v3",
 			Description: "Run a load test",
 			Steps: []*worker.WorkflowStep{
 				worker.Fn(func(ctx worker.HatchetContext) (result *stepOneOutput, err error) {
@@ -77,10 +97,13 @@ func run() (func() error, error) {
 					log.Printf("start")
 					timeStart := time.Now()
 
-					testCmd := "./loadtest loadtest --duration \"20s\" --events 10"
+					// testCmd := "./loadtest loadtest --duration \"10s\" --events 1"
+					testCmd := "echo \"Hello, World!\""
 					// run a load test
+					commandCtx, cancel := context.WithCancel(context.Background())
+					defer cancel()
 
-					cmd := exec.Command("sh", "-c", testCmd)
+					cmd := exec.CommandContext(commandCtx, "sh", "-c", testCmd)
 
 					// Inherit the current environment
 					cmd.Env = os.Environ()
@@ -93,13 +116,16 @@ func run() (func() error, error) {
 					err = cmd.Run()
 
 					if err != nil {
+						fmt.Println("Error running command " + testCmd)
+						fmt.Println(err)
 						return nil, err
 					}
 					timeEnd := time.Now()
 
 					timeTaken := timeEnd.Sub(timeStart)
-
+					fmt.Println("Time taken: ", timeTaken)
 					return &stepOneOutput{Message: fmt.Sprintf("%s", timeTaken)}, nil
+
 				},
 				).SetName("step-one"),
 			},
@@ -108,29 +134,6 @@ func run() (func() error, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error registering workflow: %w", err)
 	}
-
-	go func() {
-		testEvent := loadTestEvent{
-
-			Data: map[string]string{
-				"test": "test",
-			},
-		}
-
-		log.Printf("pushing event hatchet:load-test")
-		// push an event
-		err := c.Event().Push(
-			context.Background(),
-			"hatchet:load-test",
-			testEvent,
-			client.WithEventMetadata(map[string]string{
-				"hello": "loadtest",
-			}),
-		)
-		if err != nil {
-			panic(fmt.Errorf("error pushing event: %w", err))
-		}
-	}()
 
 	cleanup, err := w.Start()
 	if err != nil {
