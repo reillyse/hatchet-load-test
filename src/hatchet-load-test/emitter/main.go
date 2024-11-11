3bbdbe9ed829a926d4fbc075433378c49b26bb7b
Sean Reilly <sean@hatchet.run>
2024-11-11 14:30:30 -0800

    need to retry in the code now that we aren't retrying outside it

--- src/hatchet-load-test/emitter/main.go
+++ src/hatchet-load-test/emitter/main.go
@@ -36,6 +36,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	cleanup, err := run(c)
 	if err != nil {
 		panic(err)
@@ -92,83 +93,101 @@ func run(c client.Client) (func() error, error) {
 		return nil, fmt.Errorf("error creating worker: %w", err)
 	}
 
-	err = w.RegisterWorkflow(
-		&worker.WorkflowJob{
-			On:          worker.Events("hatchet:ha:load-test:v2"),
-			Name:        "ha-loadtester-v3",
-			Description: "Run a load test",
-			Concurrency: worker.Expression("'default'").MaxRuns(1).LimitStrategy(types.GroupRoundRobin),
-			Steps: []*worker.WorkflowStep{
-				worker.Fn(func(ctx worker.HatchetContext) (result *stepOneOutput, err error) {
-					input := &loadTestEvent{}
+	// we need to retry the workflow if it is unavailable
+	retryCount := 10
+	for {
 
-					err = ctx.WorkflowInput(input)
+		err = w.RegisterWorkflow(
+			&worker.WorkflowJob{
+				On:          worker.Events("hatchet:ha:load-test:v2"),
+				Name:        "ha-loadtester-v3",
+				Description: "Run a load test",
+				Concurrency: worker.Expression("'default'").MaxRuns(1).LimitStrategy(types.GroupRoundRobin),
+				Steps: []*worker.WorkflowStep{
+					worker.Fn(func(ctx worker.HatchetContext) (result *stepOneOutput, err error) {
+						input := &loadTestEvent{}
 
-					if err != nil {
-						return nil, err
-					}
+						err = ctx.WorkflowInput(input)
 
-					log.Printf("start")
-					timeStart := time.Now()
+						if err != nil {
+							return nil, err
+						}
 
-					events := os.Getenv("HATCHET_LOADTEST_EVENTS")
-					duration := os.Getenv("HATCHET_LOADTEST_DURATION")
+						log.Printf("start")
+						timeStart := time.Now()
 
-					if events == "" {
-						events = "1"
-					}
+						events := os.Getenv("HATCHET_LOADTEST_EVENTS")
+						duration := os.Getenv("HATCHET_LOADTEST_DURATION")
 
-					if duration == "" {
-						duration = "10s"
-					}
+						if events == "" {
+							events = "1"
+						}
 
-					if input.Data["duration"] != "" {
-						duration = input.Data["duration"]
-						log.Printf("using duration from input data: %s", duration)
-					}
+						if duration == "" {
+							duration = "10s"
+						}
 
-					if input.Data["events"] != "" {
-						events = input.Data["events"]
-						log.Printf("using events from input data: %s", events)
-					}
+						if input.Data["duration"] != "" {
+							duration = input.Data["duration"]
+							log.Printf("using duration from input data: %s", duration)
+						}
 
-					// for container it is in root
-					testCmd := fmt.Sprintf("/loadtest loadtest --duration \"%s\" --events %s", duration, events)
-					//testCmd := "echo \"Hello, World!\""
-					// run a load test
-					commandCtx, cancel := context.WithCancel(context.Background())
-					defer cancel()
+						if input.Data["events"] != "" {
+							events = input.Data["events"]
+							log.Printf("using events from input data: %s", events)
+						}
 
-					cmd := exec.CommandContext(commandCtx, "sh", "-c", testCmd)
+						// for container it is in root
+						testCmd := fmt.Sprintf("/loadtest loadtest --duration \"%s\" --events %s", duration, events)
+						//testCmd := "echo \"Hello, World!\""
+						// run a load test
+						commandCtx, cancel := context.WithCancel(context.Background())
+						defer cancel()
 
-					// Inherit the current environment
-					cmd.Env = os.Environ()
+						cmd := exec.CommandContext(commandCtx, "sh", "-c", testCmd)
 
-					// Set the output to go to the standard output and error
-					cmd.Stdout = os.Stdout
-					cmd.Stderr = os.Stderr
+						// Inherit the current environment
+						cmd.Env = os.Environ()
 
-					// Run the command
-					err = cmd.Run()
+						// Set the output to go to the standard output and error
+						cmd.Stdout = os.Stdout
+						cmd.Stderr = os.Stderr
 
-					if err != nil {
-						fmt.Println("Error running command " + testCmd)
-						fmt.Println(err)
-						return nil, err
-					}
-					timeEnd := time.Now()
+						// Run the command
+						err = cmd.Run()
 
-					timeTaken := timeEnd.Sub(timeStart)
-					fmt.Println("Time taken: ", timeTaken)
-					return &stepOneOutput{Message: fmt.Sprintf("%s", timeTaken)}, nil
+						if err != nil {
+							fmt.Println("Error running command " + testCmd)
+							fmt.Println(err)
+							return nil, err
+						}
+						timeEnd := time.Now()
 
+						timeTaken := timeEnd.Sub(timeStart)
+						fmt.Println("Time taken: ", timeTaken)
+						return &stepOneOutput{Message: fmt.Sprintf("%s", timeTaken)}, nil
+
+					},
+					).SetName("step-one").SetRetries(0),
 				},
-				).SetName("step-one").SetRetries(0),
 			},
-		},
-	)
-	if err != nil {
-		return nil, fmt.Errorf("error registering workflow: %w", err)
+		)
+
+		if err == nil {
+			break
+		}
+
+		if retryCount > 0 {
+			retryCount--
+			time.Sleep(5 * time.Second)
+			log.Printf("got error %s retrying %d more times", err, retryCount)
+		} else {
+			break
+		}
+
+		if err != nil && retryCount == 0 {
+			return nil, fmt.Errorf("error registering workflow: %w", err)
+		}
 	}
 
 	cleanup, err := w.Start()
